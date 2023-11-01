2edf1fcca8740b84507883b5899651dc8175ae7f
Naima Takiouti <106370938+naimataki@users.noreply.github.com>
2023-11-01 22:16:03 +0100

    git commit - m "v2 update"

--- v2.py
+++ v2.py
@@ -5,9 +5,9 @@ from torch.nn import functional as F
 # hyperparameters
 batch_size = 32 # how many independent sequences will we process in parallel?
 block_size = 8 #what is the maximum context length for predictions?
-max_iters = 3000
+max_iters = 5000
 eval_interval = 300
-learning_rate = 1e-2
+learning_rate = 1e-3
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
 n_embd = 32
@@ -55,7 +55,71 @@ def estimate_loss():
             losses[k] = loss.item()
         out[split] = loss.item()
     model.train()
-    return out           
+    return out   
+
+class Head(nn.Module):
+    # one head of self-attention   
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embd, head_size, bias=False)
+        self.query = nn.Linear(n_embd, head_size, bias=False)
+        self.value = nn.Linear(n_embd, head_size, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+
+    def forward(self, x):
+        B, T, C = x.shape
+        k = self.key(x) # (B, T, C)
+        q = self.query(x) # (B, T, C)
+        # compute attention scores ("affinities")
+        wei = q @ k.transpose(-1, -2) * C**-0.5 # (B, T, T)
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (T, T)
+        wei = F.softmax(wei, dim=-1) # (B, T, T)
+        # apply attention to values
+        v = self.value(x) # (B, T, C)
+        out = wei @ v # (B, T, C)
+        return out
+    
+class MultiHeadAttention(nn.Module):
+    # mulpiple heads of self-attention in parallel
+
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(n_embd, n_embd)
+
+    def forward(self, x):
+        # x is (B, T, C)
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.proj(out)
+        return out
+    
+class FeedForward(nn.Module):
+    # a simple linear layer with a nonlinearity
+
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, 4 * n_embd),
+            nn.ReLU(),
+            nn.Linear(4 * n_embd, n_embd),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+    
+class Block(nn.Module):
+    # one transformer block: communication followed by computation
+    def __init__(self, n_embd, n_head):
+        # n_embd is the dimensionality of the embedding: the number we'd like
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedForward(n_embd)
+
+    def forward(self, x):
+        x = x + self.sa(x)
+        x = x + self.ffwd(x)
+        return x
 
 class BiagramLanguageModel(nn.Module):
     
@@ -64,6 +128,14 @@ class BiagramLanguageModel(nn.Module):
         # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
+        #self.sa_head = Head(n_embd)
+        #self.sa_heads = MultiHeadAttention(4, n_embd//4) # i.e 4 heads of 8-dim self-attention
+        #self.ffwd = FeedForward(n_embd)
+        self.blocks = nn.Sequential(
+            Block(n_embd, n_head=4),
+            Block(n_embd, n_head=4),
+            Block(n_embd, n_head=4),
+        )
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -73,6 +145,9 @@ class BiagramLanguageModel(nn.Module):
         tok_emb = self.token_embedding_table(idx) # (B, T, C)
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T, C)
         x = tok_emb + pos_emb # (B, T, C)
+        #x = self.sa_heads(x) # apply one head of self attention. (B, T, C)
+        #x = self.ffwd(x) # apply feed forward layer. (B, T, C)
+        x = self.blocks(x)
         logits = self.lm_head(x) # (B, T, vocab_size)
 
         if targets is None:
@@ -87,8 +162,10 @@ class BiagramLanguageModel(nn.Module):
     def generate(self, idx, max_new_tokens):
         # idx is (B, T) array of indices in the current context
         for _ in range(max_new_tokens):
+            # crop idx to the last block_size tokens
+            idx_cond = idx[:, -block_size:]
             #get the predictions
-            logits, loss = self(idx)
+            logits, loss = self(idx_cond)
             # focus only on the last time step
             logits = logits[:, -1, :] # becomes(B, C)
             # apply softmax to get probabilities
