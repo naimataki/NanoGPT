2bee65eeaf78dba9d386ac5972c5711b3bf4d245
Naima Takiouti <106370938+naimataki@users.noreply.github.com>
2023-11-02 06:06:56 +0100

    final commit

--- v2.py
+++ v2.py
@@ -3,14 +3,17 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # hyperparameters
-batch_size = 32 # how many independent sequences will we process in parallel?
-block_size = 8 #what is the maximum context length for predictions?
+batch_size = 64 # how many independent sequences will we process in parallel?
+block_size = 256 #what is the maximum context length for predictions?
 max_iters = 5000
-eval_interval = 300
-learning_rate = 1e-3
+eval_interval = 500
+learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 32
+n_embd = 384 # embedding dimension
+n_head = 6
+n_layer = 6
+dropout = 0.2
 # -------------
 
 torch.manual_seed(1337)
@@ -66,6 +69,8 @@ class Head(nn.Module):
         self.value = nn.Linear(n_embd, head_size, bias=False)
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
 
+        self.dropout = nn.Dropout(dropout)
+
     def forward(self, x):
         B, T, C = x.shape
         k = self.key(x) # (B, T, C)
@@ -74,6 +79,7 @@ class Head(nn.Module):
         wei = q @ k.transpose(-1, -2) * C**-0.5 # (B, T, T)
         wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (T, T)
         wei = F.softmax(wei, dim=-1) # (B, T, T)
+        wei = self.dropout(wei) # (B, T, T)
         # apply attention to values
         v = self.value(x) # (B, T, C)
         out = wei @ v # (B, T, C)
@@ -86,6 +92,7 @@ class MultiHeadAttention(nn.Module):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
         self.proj = nn.Linear(n_embd, n_embd)
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         # x is (B, T, C)
@@ -102,6 +109,7 @@ class FeedForward(nn.Module):
             nn.Linear(n_embd, 4 * n_embd),
             nn.ReLU(),
             nn.Linear(4 * n_embd, n_embd),
+            nn.Dropout(dropout),
         )
 
     def forward(self, x):
@@ -115,10 +123,12 @@ class Block(nn.Module):
         head_size = n_embd // n_head
         self.sa = MultiHeadAttention(n_head, head_size)
         self.ffwd = FeedForward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
 
     def forward(self, x):
-        x = x + self.sa(x)
-        x = x + self.ffwd(x)
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
         return x
 
 class BiagramLanguageModel(nn.Module):
@@ -131,11 +141,8 @@ class BiagramLanguageModel(nn.Module):
         #self.sa_head = Head(n_embd)
         #self.sa_heads = MultiHeadAttention(4, n_embd//4) # i.e 4 heads of 8-dim self-attention
         #self.ffwd = FeedForward(n_embd)
-        self.blocks = nn.Sequential(
-            Block(n_embd, n_head=4),
-            Block(n_embd, n_head=4),
-            Block(n_embd, n_head=4),
-        )
+        self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)]) 
+        self.ln_f = nn.LayerNorm(n_embd) # final layer norm
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -177,7 +184,9 @@ class BiagramLanguageModel(nn.Module):
         return idx
 
 model = BiagramLanguageModel()
-m = model.to(device) 
+m = model.to(device)
+# print the number of parameters in the model
+print(sum([p.numel() for p in m.parameters()])/1e6, "M parameters")
 
 # create the optimizer
 optimizer = torch.optim.AdamW(m.parameters(), lr=learning_rate)
